d9ca1ccddca8929953d6dbccb99452e5e30efea7
Gabriel Souza <104937852+gabrielsouzas@users.noreply.github.com>
2022-09-02 17:27:34 -0300

    Novo modelo portfolio

--- js/script.js
+++ js/script.js
@@ -1,44 +1,5 @@
-/* Navegação dos links do menu */
-                                           /* Pega apenas os links com o valor do href iniciado em # */
-const menuItens = document.querySelectorAll('.menu a[href^="#"]')
+const header = document.querySelector("header");
 
-/* Eventos de clique dos links */
-menuItens.forEach(item => {
-    item.addEventListener('click', scrollToIdOnClick)
-})
-
-function scrollToIdOnClick(event) {
-    event.preventDefault();
-    const to = getScrollTopByHref(event.target) - 40
-    scrollToPosition(to)
-}
-
-function getScrollTopByHref(element) {
-    const id = element.getAttribute('href')
-    return document.querySelector(id).offsetTop
-}
-
-function scrollToPosition(to) {
-    window.scroll({
-        top: to,
-        behavior: "smooth",
-    })
-}
-
-/* Estrelas */
-
-const star = document.querySelector('.stars')
-
-var shadowConstruct = ""
-for (let i = 0; i < 10; i++) {
-    shadowConstruct += `${getRandom(500)}px ${getRandom(500)}px white,`
-}
-
-const shadowFinal = shadowConstruct.substring(0, shadowConstruct.length - 1);
-
-star.style.boxShadow = shadowFinal
-
-/* Gera um número aleatório inteiro passando o valor máximo */
-function getRandom(max) {
-    return Math.floor(Math.random() * max + 1)
-}
+window.addEventListener("scroll", function() {
+    header.classList.toggle("sticky", this.window.scrollY > 0)
+});
