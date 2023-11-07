1e63ea46e6835f7bad82873e5707700139a65bac
Gabriel Souza <104937852+gabrielsouzas@users.noreply.github.com>
2023-11-07 16:22:37 -0300

    Correção responsividade

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,25 +2,28 @@ let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
+  menu.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
 };
 
 window.onscroll = () => {
-    menu.classList.remove('bx-x');
-    navbar.classList.remove('active');
+  menu.classList.remove('bx-x');
+  navbar.classList.remove('active');
 };
 
 /* SCROLLREVEAL */
 
-const sr = ScrollReveal ({
-    distance: '60px',
-    duration: 2500,
-    reset: true
-})
+const sr = ScrollReveal({
+  distance: '60px',
+  duration: 1000,
+  reset: true,
+});
 
-sr.reveal('.home', {delay:300, origin:'top'})
+sr.reveal('.home', { delay: 50, origin: 'top' });
 //sr.reveal('.home-text', {delay:200, origin:'top'})
 //sr.reveal('.home-habilidades', {delay:200, origin:'top'})
 //sr.reveal('.home-img', {delay:400, origin:'top'})
-sr.reveal('.sobre, .servicos, .cta, .curriculo, .contato, .copyright', {delay:200, origin:'top'})
+sr.reveal('.sobre, .servicos, .cta, .curriculo, .contato, .copyright', {
+  delay: 50,
+  origin: 'top',
+});
